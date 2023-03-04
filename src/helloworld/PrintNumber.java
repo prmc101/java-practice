c06deb6a9f8077270caa5f4d5f94a8980507a961
prmc101 <primecommunity101@gmail.com>
2023-03-04 17:36:36 +0000

    Today's class update

--- src/helloworld/PrintNumber.java
+++ src/helloworld/PrintNumber.java
@@ -11,7 +11,10 @@ public class PrintNumber {
         String name = "Prime Testing";
         System.out.println(a); // 10
         System.out.println(b); // 20
+        System.out.println(30); // 30
+        System.out.println(10+40); // 50
+        System.out.println(name); // Prime Testing
         System.out.println(a+b); // 30
-        System.out.println(name);
+        System.out.println(a*b); // 200
     }
 }
