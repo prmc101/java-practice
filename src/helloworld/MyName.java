37bb6c89dcf8bb903fceaeb1102ee6680c877d7d
prmc101 <primecommunity101@gmail.com>
2023-03-04 15:43:17 +0000

    Today's class update

---
+++ src/helloworld/MyName.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class MyName {
+
+    public static void main(String[] args) {
+        System.out.println("Jay");
+        System.out.println("Vaghani");
+    }
+}
---
+++ src/helloworld/PrintNumber.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class PrintNumber {
+    public static void main(String[] args) {
+        //format - Ctrl+Alt+l
+        int a = 10;
+        int b = 20;
+        String name = "Prime Testing";
+        System.out.println(a); // 10
+        System.out.println(b); // 20
+        System.out.println(a+b); // 30
+        System.out.println(name);
+    }
+}
