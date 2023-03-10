a67288bb6cec792ac60c82fd7e454201f60837e9
amitdptl <amitkathrotiya@gmail.com>
2023-03-10 16:58:05 +0000

    name changed

--- src/helloworld/MyName.java
+++ src/helloworld/MyName.java
@@ -6,7 +6,7 @@ package helloworld;
 public class MyName {
 
     public static void main(String[] args) {
-        System.out.println("Jay");
-        System.out.println("Vaghani");
+        System.out.println("Amit");
+        System.out.println("Kathrotiya");
     }
 }
